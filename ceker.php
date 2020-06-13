4f800b8c444748dbcf3be47a63013ff3b95a3014
hanherdian1305 <hanherdian1305@gmail.com>
2020-06-13 13:07:55 +0700

    ceker.php

--- ceker.php
+++ ceker.php
@@ -9,59 +9,6 @@ $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = 'X-Location: id_ID';
 $header[] ='Authorization: Bearer '.$token;
 $header[] = 'pin:'.$pin.'';
-
-
-echo color("nevy","\n▬▬▬▬▬▬▬▬▬▬▬▬* SANTUY DULU KAWAN *▬▬▬▬▬▬▬▬▬▬▬▬");
-        echo "\n".color("nevy","# CLAIM VOUCHER GOFOOD ");
-        echo "\n".color("yellow","# TUNGGU YA ");
-        for($a=1;$a<=3;$a++){
-        echo color("yellow",".");
-        sleep(20);
-        }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"EATLAH"}');
-        $message = fetch_value($code1,'"message":"','"');
-        if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
-        echo "\n".color("green","🔓▶️ Message: ".$message);
-        goto gocar;
-        }else{
-        echo "\n".color("red","🔐▶️ Message: ".$message);
-	      gocar:
-        echo "\n".color("nevy","# CLAIM FOOD Lain");
-        echo "\n".color("yellow","# Google luck ");
-        for($a=1;$a<=3;$a++){
-        echo color("yellow",".");
-        sleep(15);
-        }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"HOPHOP"}');
-        $message = fetch_value($code1,'"message":"','"');
-        if(strpos($code1, 'Promo kamu sudah bisa dipakai.')){
-        echo "\n".color("green","🔓▶️ Message: ".$message);
-        goto gofood;
-        }else{
-        echo "\n".color("red","🔐▶️ Message: ".$message);
-        gofood:
-        echo "\n".color("nevy","# CLAIM ULANG");
-        echo "\n".color("yellow","SAMPE CROT");
-        for($a=1;$a<=3;$a++){
-        echo color("yellow",".");
-        sleep(15);
-        }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"STAYGOFOOD201105SC"}');
-        $message = fetch_value($code1,'"message":"','"');
-        echo "\n".color("green","🔓▶️ Message: ".$message);
-        echo "\n".color("nevy","# CLAIM LAGI GOFOOD 15+10+5k");
-        echo "\n".color("yellow","# SABAR YA , DIKIT LAGI KOK BIAR CROT");
-        for($a=1;$a<=3;$a++){
-        echo color("yellow",".");
-        sleep(5);
-        }
-        sleep(15);
-        $boba09 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOODLAGI0906"}');
-        $messageboba09 = fetch_value($boba09,'"message":"','"');
-        echo "\n".color("green","# Message: ".$messageboba09);
-        sleep(3);
-
-
 //CHECKER DETAIL AKUN
 $info = curl('https://api.gojekapi.com/v1/chat/profile', null, $header);
     $verifs = json_decode($info[0]);
